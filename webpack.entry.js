18b9e01f7251e17268a6a5029ad47378a54ec785
Henri Bergius <henri.bergius@iki.fi>
2020-09-28 14:18:35 +0200

    Initial: un-Grunt

---
+++ webpack.entry.js
@@ -0,0 +1,13 @@
+const exported = {
+  // eslint-disable-next-line import/no-extraneous-dependencies
+  noflo: require('noflo'),
+};
+
+if (window) {
+  window.require = function (moduleName) {
+    if (exported[moduleName]) {
+      return exported[moduleName];
+    }
+    throw new Error(`Module ${moduleName} not available`);
+  };
+}
