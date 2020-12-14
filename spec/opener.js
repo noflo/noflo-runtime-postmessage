941b095712be3d1ae63ff82a2c1c87ee9b5c660f
Henri Bergius <henri.bergius@iki.fi>
2020-12-14 15:46:19 +0100

    Wait for the graph to be initialized before continuing with the tests

--- spec/opener.js
+++ spec/opener.js
@@ -62,41 +62,48 @@ describe('OPENER network runtime', () => {
 
   describe('Graph Protocol', () => {
     describe('receiving a graph and nodes', () => it('should provide the nodes back', (done) => {
-      const expects = [{
-        protocol: 'graph',
-        command: 'clear',
-      },
-      {
-        protocol: 'graph',
-        command: 'addnode',
-        payload: {
-          id: 'Foo',
-          component: 'core/Repeat',
-          metadata: {
-            hello: 'World',
+      const expects = [
+        {
+          protocol: 'graph',
+          command: 'addnode',
+          payload: {
+            id: 'Foo',
+            component: 'core/Repeat',
+            metadata: {
+              hello: 'World',
+            },
+            graph: 'foo',
           },
-          graph: 'foo',
         },
-      },
-      {
-        protocol: 'graph',
-        command: 'addnode',
-        payload: {
-          id: 'Bar',
-          component: 'core/Drop',
-          metadata: {},
-          graph: 'foo',
+        {
+          protocol: 'graph',
+          command: 'addnode',
+          payload: {
+            id: 'Bar',
+            component: 'core/Drop',
+            metadata: {},
+            graph: 'foo',
+          },
         },
-      },
       ];
-      receive('graph', expects, done);
+      receive('graph', [
+        {
+          protocol: 'graph',
+          command: 'clear',
+        },
+      ], (err) => {
+        if (err) {
+          done(err);
+          return;
+        }
+        send('graph', 'addnode', expects[1].payload);
+        send('graph', 'addnode', expects[2].payload);
+      });
       send('graph', 'clear', {
         baseDir: '/noflo-runtime-iframe',
         id: 'foo',
         main: true,
       });
-      send('graph', 'addnode', expects[1].payload);
-      send('graph', 'addnode', expects[2].payload);
     }));
     describe('receiving an edge', () => it('should provide the edge back', (done) => {
       const expects = [{
